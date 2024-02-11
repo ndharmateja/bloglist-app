8b26e310687fe16d7988170bbadc1347e532119e
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2024-02-11 18:41:35 +0530

    add backup port

--- backend/utils/config.js
+++ backend/utils/config.js
@@ -1,10 +1,10 @@
 require("dotenv").config();
 const process = require("process");
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3001;
 const MONGODB_URI = process.env.MONGODB_URI;
 
 module.exports = {
-    MONGODB_URI,
     PORT,
+    MONGODB_URI,
 };
