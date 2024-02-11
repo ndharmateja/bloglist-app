a283c65738dcb2bfaec5eb306560aa44a0309adf
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2024-02-11 18:37:29 +0530

    create index.js

---
+++ backend/index.js
@@ -0,0 +1,22 @@
+const process = require("process");
+const express = require("express");
+const cors = require("cors");
+const mw = require("./utils/middleware");
+const { info } = require("./utils/logger");
+
+// Create express app
+const app = express();
+
+// middleware
+app.use(express.json());
+app.use(mw.requestLogger);
+app.use(express.static("dist"));
+app.use(cors());
+
+// Not found route and error handler
+app.use(mw.notFoundRoute);
+app.use(mw.errorHandler);
+
+// Start listening
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => info(`Server started. Listening on ${PORT}.`));
