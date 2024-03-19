184b55cf90cb24ce2a7ba509cd09ce6e922bd9f2
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2024-03-19 08:25:51 +0530

    uibuild

--- backend/app.js
+++ backend/app.js
@@ -26,12 +26,14 @@ mongoose
 app.use(cors());
 app.use(express.json());
 app.use(mw.requestLogger);
-app.use(express.static("dist"));
 
 // Routes
 app.use("/api/blogs", blogsRouter);
 app.use("/api/users", usersRouter);
 app.use("/api/login", loginRouter);
+
+// Frontend
+app.use(express.static("dist"));
 
 // Not found route and error handler middleware
 app.use(mw.notFoundRoute);
