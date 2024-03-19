365c236a99c47fdd602a2d23bf79e7fa9bd0d452
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2024-03-19 13:39:57 +0530

    add likes and comments

--- frontend/src/components/blogs/BlogsList.jsx
+++ frontend/src/components/blogs/BlogsList.jsx
@@ -1,22 +1,33 @@
 import { Link } from "react-router-dom";
-import Blog from "./Blog";
-import { useSelector } from "react-redux";
+import { FaComment, FaThumbsUp } from "react-icons/fa";
+import { useSelector, useDispatch } from "react-redux";
+import { likeBlog } from "../../reducers/blogsReducer";
 
 const BlogsList = () => {
     const blogs = useSelector((store) => store.blogs);
+    const dispatch = useDispatch();
 
+    const padding = 10;
     const blogStyle = {
-        paddingTop: 5,
-        paddingLeft: 5,
+        paddingTop: padding,
+        paddingLeft: padding,
         border: "solid",
         borderWidth: 1,
         marginBottom: 5,
-        paddingBottom: 5,
+        paddingBottom: padding,
+        paddingRight: padding,
     };
 
-    return blogs.map(({ id, title }) => (
+    return blogs.map(({ id, title, likes, comments }) => (
         <div style={blogStyle} key={id}>
             <Link to={`/blogs/${id}`}>{title}</Link>
+            <div
+                onClick={() => dispatch(likeBlog(id))}
+                style={{ float: "right", color: "black" }}
+            >
+                {likes} <FaThumbsUp />
+                &nbsp; {comments.length} <FaComment />
+            </div>
         </div>
     ));
 };
